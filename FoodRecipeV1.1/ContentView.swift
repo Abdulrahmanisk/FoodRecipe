6fe701942662cea6bc8835413395a6b62f8c0063
Abdulrahman <Abdulrhman.isk@gmail.com>
2023-12-23 19:43:32 +0300

    Initial Commit

---
+++ FoodRecipeV1.1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodRecipeV1.1
+//
+//  Created by Abdulrahman isk on 23/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodRecipeV1.1/FoodRecipeV1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipeV1_1App.swift
+//  FoodRecipeV1.1
+//
+//  Created by Abdulrahman isk on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipeV1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
